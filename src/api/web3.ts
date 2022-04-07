e84346205e593bdce079bf25629de8a73ac7c16a
JoE11-y <blockydevjoe@gmail.com>
2022-04-07 23:34:45 +0100

    updating the web3 functions

--- src/api/web3.ts
+++ src/api/web3.ts
@@ -1,35 +1,10 @@
-// import detectEthereumProvider from '@metamask/detect-provider';
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 export async function connectWalletMetamask() {
     //@ts-ignore
     const { ethereum } = window;
 
-    if (!ethereum) {
-        toast.warn("Make sure you have MetaMask Connected", {
-            position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-        });
-        return
-    }
     const accounts = await ethereum.request({
         method: "eth_requestAccounts",
     });
-
-    // await ethereum.request({ 
-    //     //137 mainnet 0x89
-    //     //80001 0x13881
-    //     method: "wallet_switchEthereumChain", params:[{chainId: '0x13881'}]
-    // });
-
-    // await ethereum.enable();
-    // const accounts = await web3.eth.getAccounts();
 
     return { account: accounts[0] || "" };
 }
@@ -41,10 +16,17 @@ export function subscribeToAccount(
         try {
             // @ts-ignore
             const { ethereum } = window;
-            const accounts = await ethereum.request({
-                method: "eth_requestAccounts",
-            });
-            callback(null, accounts[0])
+
+            const check = await ethereum._metamask.isUnlocked();
+
+            if (check) {
+                const accounts = await ethereum.request({
+                    method: "eth_requestAccounts",
+                });
+                callback(null, accounts[0])
+            } else {
+                callback(null, "locked")
+            }
         } catch (e) {
             const error = e as Error;
             callback(error, "")
@@ -54,6 +36,68 @@ export function subscribeToAccount(
     return () => {
         clearInterval(id);
     };
+}
+
+
+export function getChainID(
+    callback: (error: Error | null, chainId: string | "") => any
+) {
+    const id = setInterval(async () => {
+        try {
+            // @ts-ignore
+            const { ethereum } = window;
+
+            const chainId = await ethereum.request({ method: 'eth_chainId' });
+            callback(null, chainId)
+        } catch (e) {
+            const error = e as Error;
+            callback(error, "")
+        }
+    }, 1000);
+
+    return () => {
+        clearInterval(id);
+    };
+}
+
+export async function subscribeToChainID() {
+    try {
+        // @ts-ignore
+        const { ethereum } = window;
+        await ethereum.request({
+            //137 mainnet 0x89
+            //80001 0x13881
+            method: "wallet_switchEthereumChain", params: [{ chainId: '0x13881' }]
+        });
+    } catch (switchError) {
+        // This error code indicates that the chain has not been added to MetaMask.
+        // @ts-ignore
+        if (switchError.code === 4902) {
+            try {
+                // @ts-ignore
+                const { ethereum } = window;
+                await ethereum.request({
+                    method: 'wallet_addEthereumChain',
+                    params: [
+                        {
+                            chainId: '0x13881',
+                            chainName: 'Mumbai Testnet',
+                            rpcUrls: ['https://rpc-mumbai.matic.today'],
+                            nativeCurrency: {
+                                name: "MATIC",
+                                symbol: "MATIC", // 2-6 characters long
+                                decimals: 18,
+                            },
+                            blockExplorerUrls: ["https://mumbai.polygonscan.com"]
+                        },
+                    ],
+                });
+            } catch (addError) {
+                console.error(addError);
+            }
+        }
+    }
+
 }
 
 
